3131ed649df071f5799febe2de5ed6ecc29a3359
Samuel <sgarciaanato@gmail.com>
2020-08-01 11:57:58 -0400

    change searchbar behavior

--- Counters/Modules/Main/Delegates/MainViewSearchBarDelegate.swift
+++ Counters/Modules/Main/Delegates/MainViewSearchBarDelegate.swift
@@ -13,7 +13,7 @@ extension MainView : UISearchBarDelegate {
     func configureSearchBar() {
         search.searchBar.delegate = self
         search.searchBar.sizeToFit()
-        search.obscuresBackgroundDuringPresentation = true
+        search.obscuresBackgroundDuringPresentation = false
         search.hidesNavigationBarDuringPresentation = true
         self.definesPresentationContext = true
         search.searchBar.placeholder = "Search"
@@ -25,6 +25,10 @@ extension MainView : UISearchBarDelegate {
     
     @objc func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         controller?.fetchCounters(searchText: searchText)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        controller?.fetchCounters(searchText: "")
     }
     
     override func viewSafeAreaInsetsDidChange() {
